5e6182cd34783206caa02ae7a84b722f5620465d
minjilee-03 <mj0524321@naver.com>
2022-03-16 16:14:42 +0900

    feat:create-button-use-RaisedButton

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,30 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite color?',
+      'what\'s your favorite animarl',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My Frist App'),
         ),
-        body: Text('Hello World!'),
+        body: Column(children: [
+          Text('the question!'),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: null,
+          ),
+        ]),
       ),
     );
   }
